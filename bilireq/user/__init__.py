d03253f6f0a58d3a4e611d8036ee9476ac4b4b32
Evilmass <21218296+Evilmass@users.noreply.github.com>
2024-11-19 21:43:28 +0800

    [fix]-352 error at get_user_info api

--- bilireq/user/__init__.py
+++ bilireq/user/__init__.py
@@ -7,7 +7,7 @@ BASE_URL = "https://api.bilibili.com"
 
 async def get_user_info(uid: Union[int, str], *, auth=None, reqtype="app", **kwargs):
     """根据 UID 获取指定用户信息"""
-    url = f"{BASE_URL}/x/space/wbi/acc/info"
+    url = f"{BASE_URL}/x/web-interface/card"
     params = {"mid": uid}
     return await get(
         url, params=params, auth=auth, reqtype=reqtype, is_wbi=True, **kwargs
